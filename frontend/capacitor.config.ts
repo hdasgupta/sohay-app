cc208606f72402813e2acb2e4e287a1967b72579
Himaghna Dasgupta <himaghna.dasgupta@gmail.com>
2026-09-24 22:57:08 +0530

    capacitor.config.ts update

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sohay.app',
+  appName: 'Sohay',
+  webDir: 'dist'
+};
+
+export default config;
