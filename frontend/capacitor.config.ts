5a77a7931142b5dfc99d2c982e04301fe7b395ad
Himaghna Dasgupta <himaghna.dasgupta@gmail.com>
2026-09-25 01:25:33 +0530

    Add capacitor.config.json pointing to dist

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.sohay.app',
-  appName: 'Sohay App',
-  webDir: 'build'
-};
-
-export default config;
